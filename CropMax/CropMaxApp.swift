86fe2f5b9a773c7ed33377e91748fee6dd1a5784
Xander Wanagel <xander.wanagel@gmail.com>
2025-03-20 00:23:15 +0100

    Initial Commit

---
+++ CropMax/CropMaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  CropMaxApp.swift
+//  CropMax
+//
+//  Created by Xander Wanagel on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct CropMaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
